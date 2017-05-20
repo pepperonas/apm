bcf7ff6651efb119d22f22e7ce769e434a012855
pepperonas <martinpaush@gmail.com>
2017-05-21 01:47:55 +0200

    first beta

--- src/io/celox/Main.java
+++ src/io/celox/Main.java
@@ -20,20 +20,22 @@ package io.celox;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -60,93 +62,44 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    @SuppressWarnings("unused")
     private static final String TAG = "Main";
+
+    private TextField tfPackageName;
+    private ChoiceBox<String> choiceBoxPermissions;
+
+    private static final String ANDROID_PERMISSION = "android.permission.";
+    private ListView<String> listView;
+    private Button btnGetPermissions;
+    private String lastSelectedPermission = "";
 
     @Override
     public void start(Stage stage) throws Exception {
+        @SuppressWarnings("unused")
         Parent root = FXMLLoader.load(getClass().getResource("layout_main.fxml"));
+
         stage.setTitle(Const.APP_NAME);
 
-        TextField tfPackageName = new TextField();
-        tfPackageName.setText(Prefs.getLastPackageName());
-        tfPackageName.setPromptText("Enter package name...");
+        visitPlayground();
 
-        Label lOutput = new Label("...");
-        lOutput.setWrapText(true);
-
-        Button btnGetPermissions = new Button("GET PERMISSIONS");
-        btnGetPermissions.setOnAction(event -> {
-            try {
-                Prefs.setLastPackageName(tfPackageName.getText());
-
-                String[] lines = runProcess("adb shell dumpsys package " + tfPackageName.getText()).split("\n");
-                StringBuilder output = new StringBuilder();
-                boolean writeOutput = false;
-                for (String line : lines) {
-                    if (line.contains("requested permissions:") && !writeOutput) {
-                        writeOutput = true;
-                    }
-                    if (line.contains("mSkippingApks:")) {
-                        writeOutput = false;
-                    }
-                    System.out.println(line);
-                    if (writeOutput) {
-                        output.append(line).append("\n");
-                    }
-                    lOutput.setText(output.toString());
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-
-        List<String> permissions = new ArrayList<>();
-        Field[] fields = Manifest.permission.class.getFields();
-        System.out.println(fields.length);
-        for (Field field : fields) {
-            String permission = "android.permission." + field.getName();
-            permissions.add(permission);
-            System.out.println(permission);
-        }
-
-
-        ObservableList<String> data = FXCollections.observableList(permissions);
-        final ChoiceBox<String> choiceBoxPermissions = new ChoiceBox<>(data);
-        choiceBoxPermissions.getSelectionModel().selectFirst();
-
-        VBox vBoxLeft = new VBox(5);
-        vBoxLeft.getChildren().addAll(tfPackageName, btnGetPermissions, lOutput);
         BorderPane borderPane = new BorderPane();
+
+        VBox vBoxLeft = getPaneLeft();
+        vBoxLeft.setPadding(new Insets(5, 5, 5, 5));
         borderPane.setLeft(vBoxLeft);
 
-        Button btnGrantPermission = new Button("GRANT PERMISSION");
-        btnGrantPermission.setOnAction(event -> {
-            try {
-                runProcess("adb shell pm grant " + tfPackageName.getText() + " " + choiceBoxPermissions.getValue());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        Button btnRevokePermission = new Button("REVOKE PERMISSION");
-        btnRevokePermission.setOnAction(event -> {
-            try {
-                runProcess("adb shell pm revoke " + tfPackageName.getText() + " " + choiceBoxPermissions.getValue());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
+        VBox vBoxCenter = getPaneCenter();
+        vBoxCenter.setPadding(new Insets(5, 5, 5, 5));
+        borderPane.setCenter(vBoxCenter);
 
-        HBox hBoxButtonsRight = new HBox(5);
-        hBoxButtonsRight.getChildren().addAll(btnGrantPermission, btnRevokePermission);
-
-        VBox vBoxRight = new VBox(5);
-        vBoxRight.getChildren().addAll(choiceBoxPermissions, hBoxButtonsRight);
+        VBox vBoxRight = getPaneRight(tfPackageName, choiceBoxPermissions);
+        vBoxRight.setPadding(new Insets(5, 5, 5, 5));
         borderPane.setRight(vBoxRight);
+        borderPane.setPadding(new Insets(10, 10, 10, 10));
 
         // layout the scene.
         final StackPane background = new StackPane();
-        background.setStyle("-fx-background-color: cornsilk;");
+        background.setStyle("-fx-background-color: #B2DFDB;");
         final Scene scene = new Scene(new Group(background, borderPane), 800, 600);
         background.prefHeightProperty().bind(scene.heightProperty());
         background.prefWidthProperty().bind(scene.widthProperty());
@@ -156,7 +109,173 @@ public class Main extends Application {
         setCloseOnEsc(scene);
     }
 
-    public String runProcess(String command) throws IOException {
+
+    private VBox getPaneLeft() {
+        listView = new ListView<>();
+        listView.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> tfPackageName.setText(newValue != null ? newValue : oldValue));
+
+        Button btnGetPackages = new Button("GET PACKAGES");
+        btnGetPackages.setOnAction(event -> {
+
+            List<String> packages = new ArrayList<>();
+            try {
+                String[] lines = runProcess("adb shell pm list packages").split("\n");
+                for (String line : lines) {
+                    if (line == null
+                            || line.isEmpty()
+                            || line.equals("package:android")
+                            || line.equals("package:x.abcd")
+                            || line.contains("package:com.android.")) {
+                        continue;
+                    }
+                    String pkg = line.replace("package:", "");
+                    packages.add(pkg);
+                    System.out.println(pkg);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Platform.runLater(() -> {
+                ObservableList<String> data = FXCollections.observableList(packages);
+                listView.getItems().setAll(data.sorted());
+
+                if (tfPackageName.getText() != null && !tfPackageName.getText().isEmpty()) {
+                    listView.getSelectionModel().select(tfPackageName.getText());
+                }
+            });
+        });
+
+        VBox vBoxLeft = new VBox(5);
+        vBoxLeft.getChildren().addAll(btnGetPackages, listView);
+
+        btnGetPackages.fire();
+
+        return vBoxLeft;
+    }
+
+    private VBox getPaneCenter() {
+        tfPackageName = new TextField();
+        tfPackageName.setText(Prefs.getLastPackageName());
+        tfPackageName.setEditable(false);
+
+        choiceBoxPermissions = new ChoiceBox<>();
+
+        Label lOutput = new Label("...");
+        lOutput.setWrapText(true);
+
+        btnGetPermissions = new Button("GET PERMISSIONS");
+        btnGetPermissions.setDisable(tfPackageName.getText().isEmpty());
+        btnGetPermissions.setOnAction(event -> {
+
+            if (choiceBoxPermissions.getItems().size() != 0) {
+                lastSelectedPermission = choiceBoxPermissions.getValue();
+            }
+
+            try {
+                Prefs.setLastPackageName(tfPackageName.getText());
+
+                String[] lines = runProcess("adb shell dumpsys package " +
+                        tfPackageName.getText()).split("\n");
+
+                List<String> requestedPermissions = new ArrayList<>();
+
+                StringBuilder output = new StringBuilder();
+                boolean writeOutput = false;
+                for (String line : lines) {
+                    if (line == null || line.isEmpty()) {
+                        continue;
+                    }
+
+                    if (line.contains("requested permissions:") && !writeOutput) {
+                        output.append("---REQUESTED PERMISSIONS---")
+                                .append("\n");
+                        writeOutput = true;
+                    }
+                    if (line.contains("install permissions:")) {
+                        output.append("\n")
+                                .append("---INSTALL PERMISSIONS---")
+                                .append("\n");
+                    }
+                    if (line.contains("mSkippingApks:")) {
+                        writeOutput = false;
+                    }
+                    System.out.println(line);
+                    if (writeOutput && line.contains(ANDROID_PERMISSION)) {
+                        String requestedPermission = line
+                                .replace(" ", "")
+                                .replace(ANDROID_PERMISSION, "");
+
+                        output.append(requestedPermission)
+                                .append("\n");
+
+                        if (!requestedPermission.contains("granted")) {
+                            requestedPermissions.add(requestedPermission);
+                        }
+                    }
+                    lOutput.setText(output.toString());
+
+                    ObservableList<String> data = FXCollections.observableList(requestedPermissions);
+                    choiceBoxPermissions.getItems().setAll(data);
+
+                    if (lastSelectedPermission.equals("")) {
+                        choiceBoxPermissions.getSelectionModel().selectFirst();
+                    } else choiceBoxPermissions.getSelectionModel().select(lastSelectedPermission);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        tfPackageName.textProperty().addListener((observable, oldValue, newValue) -> btnGetPermissions.setDisable(newValue.isEmpty()));
+
+        VBox vBoxCenter = new VBox(5);
+        vBoxCenter.getChildren().addAll(tfPackageName, btnGetPermissions, lOutput);
+        return vBoxCenter;
+    }
+
+    private VBox getPaneRight(TextField tfPackageName, ChoiceBox<String> choiceBoxPermissions) {
+        Button btnGrantPermission = new Button("GRANT PERMISSION");
+        btnGrantPermission.setDisable(tfPackageName.getText().isEmpty());
+        btnGrantPermission.setOnAction(event -> {
+            try {
+                runProcess("adb shell pm grant " + tfPackageName.getText() + " " +
+                        ANDROID_PERMISSION + choiceBoxPermissions.getValue());
+
+                btnGetPermissions.fire();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        Button btnRevokePermission = new Button("REVOKE PERMISSION");
+        btnRevokePermission.setDisable(tfPackageName.getText().isEmpty());
+        btnRevokePermission.setOnAction(event -> {
+            try {
+                runProcess("adb shell pm revoke " + tfPackageName.getText() + " " +
+                        ANDROID_PERMISSION + choiceBoxPermissions.getValue());
+
+                btnGetPermissions.fire();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        tfPackageName.textProperty().addListener((observable, oldValue, newValue) -> {
+            btnGrantPermission.setDisable(tfPackageName.getText().isEmpty());
+            btnRevokePermission.setDisable(tfPackageName.getText().isEmpty());
+        });
+
+        HBox hBoxButtonsRight = new HBox(5);
+        hBoxButtonsRight.getChildren().addAll(btnGrantPermission, btnRevokePermission);
+
+        VBox vBoxRight = new VBox(5);
+        vBoxRight.getChildren().addAll(choiceBoxPermissions, hBoxButtonsRight);
+        return vBoxRight;
+    }
+
+    private String runProcess(String command) throws IOException {
         System.out.print("command to run: " + command + "\n");
 
         StringBuilder result = new StringBuilder();
@@ -183,29 +302,6 @@ public class Main extends Application {
         return result.toString();
     }
 
-
-    private void createFolderOnDesktop() throws IOException {
-
-        ProcessBuilder builder;
-        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-            builder = new ProcessBuilder("cmd.exe", "/c", "cd \"C:\\Users\\marti\\Desktop\" && mkdir ok");
-        } else {
-            builder = new ProcessBuilder("/bin/bash", "-l", "-c", "cd \"C:\\Users\\marti\\Desktop\" && mkdir ok");
-        }
-
-        builder.redirectErrorStream(true);
-        Process p = builder.start();
-        BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
-        String line;
-        while (true) {
-            line = r.readLine();
-            if (line == null) {
-                break;
-            }
-            System.out.println(line);
-        }
-    }
-
     private void setCloseOnEsc(Scene scene) {
         scene.addEventHandler(KeyEvent.KEY_PRESSED, t -> {
             if (t.getCode() == KeyCode.ESCAPE) {
@@ -217,6 +313,11 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void visitPlayground() {
+//        Prefs.setLastPackageName("");
+//        Field[] fields = Manifest.permission.class.getFields(); // use to get all...
     }
 
 }
